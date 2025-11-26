0b182fe0369b70644e91c0e00abbe63c1e90981e
Nilsson-AvH <nlopez153@misena.edu.co>
2025-11-25 21:28:13 -0500

    [+] de agrego dev para usar npm run dev y :) en  index.js

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const express = require("express");         // Import the express module
 const app = express();                      // Create an instance of express 
 const PORT = 3000;
 
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`:) :) Server running on http://localhost:${PORT}`));
