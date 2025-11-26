1ef035983f0e35ee410012ca1dc8f383aea6a83f
Nilsson-AvH <nlopez153@misena.edu.co>
2025-11-25 21:03:54 -0500

    [+] .gitignore y otros

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log(`Project Delfos`);
+const express = require("express");         // Import the express module
+const app = express();                      // Create an instance of express 
+const PORT = 3000;
+
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
