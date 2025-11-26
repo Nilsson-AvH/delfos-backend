d2ab71074390e4bc7c8ea9bf77381393acb28436
Nilsson-AvH <nlopez153@misena.edu.co>
2025-11-25 21:45:43 -0500

    [+] index.js: definir get / get /health

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const express = require("express");         // Import the express module
+
 const app = express();                      // Create an instance of express 
 const PORT = 3000;
+
+//Definir las rutas
+app.get(`/`, (req, res) => {
+    res.send("<h1>Server in home</h1>")
+});
+
+app.get(`/health`, (req, res) => {
+    res.send("<h1>Server is running</h1>")
+});
 
 app.listen(PORT, () => console.log(`:) :) Server running on http://localhost:${PORT}`));
