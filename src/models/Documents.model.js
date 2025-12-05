42e9cb1b7405510c86b9e9b7e781f5099d48215e
Nilsson-AvH <nlopez153@misena.edu.co>
2025-12-05 15:46:19 -0500

    [$] Document.model: ampliacion atributos o estacion del Schema

--- src/models/Documents.model.js
+++ src/models/Documents.model.js
@@ -9,27 +9,57 @@ const DocumentsSchema = new Schema({
             fecha_final: Date,
             salario: Number,
             patch: String,
-            estado: { type: String, enum: ['Activo', 'Inactivo', 'Prueba'], default: 'Activo' },
             tipo_contrato: { type: String, enum: ['Prestacion de servicios', 'Laboral', 'Termino Fijo', 'Estudiante', 'Pasante', 'Obra Labor'], default: 'Termino Fijo' },
+            periodo: { type: String, enum: ['3 meses', '6 meses', '9 meses', '12 meses'], default: '12 meses' },
+            estado: { type: String, enum: ['Activo', 'Prueba', 'Inactivo'], default: 'Activo' },
+            required: false,
         }
-    ]
-    ,
+    ],
 
     securityCourse: [
         {
             fecha_inicio: Date,
             fecha_final: Date,
-            tipo_curso: { type: String, enum: ['Induccion', 'Reentrenamiento'], default: 'Induccion' },
             ECSP: String,
             patch: String,
+            tipo_curso: { type: String, enum: ['Induccion', 'Reentrenamiento'], default: 'Induccion' },
             estado: { type: String, enum: ['Vigente', 'No Vigente'], default: 'No Vigente' },
+            required: false,
         }
-    ]
-    ,
-    NUIP: {
+    ],
+    nuip: {
         patch: String,
         required: true,
     },
+    photo: [
+        {
+            fecha: Date,
+            patch: String,
+            required: true,
+        }
+    ],
+    signature: {
+        patch: String,
+        required: false,
+    },
+    cv: {
+        patch: String,
+        required: false,
+    },
+    passport: {
+        patch: String,
+        required: false,
+    },
+    bank: [
+        {
+            fecha: Date,
+            banco: String,
+            numero_cuenta: String,
+            tipo_cuenta: String,
+            patch: String,
+            required: false,
+        }
+    ],
 });
 
 export default DocumentsSchema;
