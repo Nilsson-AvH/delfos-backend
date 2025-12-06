ed31236e6a8c155853c0a9287fa1f93dc7588f5b
Nilsson-AvH <nlopez153@misena.edu.co>
2025-12-05 21:25:25 -0500

    [$] mongo.config.js: Agregagr ruta Mongo Atlas.

--- src/config/mongo.config.js
+++ src/config/mongo.config.js
@@ -1,17 +1,20 @@
 // Mongoose ODM, ORM
 // const mongoose = require(`mongoose`); esto es commonJS
 import mongoose from "mongoose";
+//TODO: Convertir la MONGODB_URI a variables de entorno
 
-const MONGODB_URI = `mongodb://localhost:27017/db-delfos`;
+// const MONGODB_URI = `mongodb://localhost:27017/db-delfos`;
+const MONGODB_URI = `mongodb+srv://nlr519com_db_user:Delfos@cluster0.74ccfpr.mongodb.net/db_delfos`;
+
 const dbConnection = async () => {
     try {
-        await mongoose.connect( MONGODB_URI , {} );
+        await mongoose.connect(MONGODB_URI, {});
         console.log(`Conectado a la base de datos`);
-    } 
+    }
     catch (error) {
         // console.error(error);    
         console.error(`Error de conexion con la base de datos :(`);
-    }    
+    }
 }
 
 // module.exports = dbConnection; esto es commonJS
